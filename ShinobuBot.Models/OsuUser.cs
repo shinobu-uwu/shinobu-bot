14a30db84213efeccb8b7425a50582505d242f94
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2021-06-01 11:49:33 -0300

    Create ShinobuBot.Models and OsuUser for database storage

---
+++ ShinobuBot.Models/OsuUser.cs
@@ -0,0 +1,9 @@
+namespace ShinobuBot.Models
+{
+    public class OsuUser
+    {
+        public int Id { get; set; }
+        public ulong DiscordId { get; set; }
+        public string OsuUsername { get; set; }
+    }
+}
