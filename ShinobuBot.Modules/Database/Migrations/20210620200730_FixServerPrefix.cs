e1fc7612a8847b6546a2698fc0514c4d8116ab9b
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2021-06-20 17:07:45 -0300

    Fix server prefix not saving to the database

---
+++ ShinobuBot.Modules/Database/Migrations/20210620200730_FixServerPrefix.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShinobuBot.Modules.Migrations
+{
+    public partial class FixServerPrefix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Prefix",
+                table: "Configurations",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prefix",
+                table: "Configurations");
+        }
+    }
+}
