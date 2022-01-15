a4f25ba6f12c3864088de0aa1e188e8e620d193f
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-01-15 13:35:27 -0300

    Remove set method from GuildId

--- ShinobuBot.Models/ServerConfiguration.cs
+++ ShinobuBot.Models/ServerConfiguration.cs
@@ -2,7 +2,7 @@ namespace ShinobuBot.Models
 {
     public class ServerConfiguration
     {
-        public ulong GuildId { get; set; }
+        public ulong GuildId { get; }
         public string Prefix { get; set; }
 
         public ServerConfiguration(ulong guildId, string prefix)
