dc4de30e22bec57471404e21479bfa7723839d0a
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-01-15 23:06:47 -0300

    Remove more boilerplate

--- ShinobuBot.Core/Program.cs
+++ ShinobuBot.Core/Program.cs
@@ -1,29 +1,19 @@
 using System;
-using System.Threading.Tasks;
+using System.Threading;
 using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using ShinobuBot.Core;
 using ShinobuBot.Core.Services;
 
-MainAsync().GetAwaiter().GetResult();
+using var services = Startup.ConfigureServices().BuildServiceProvider();
+var client = services.GetRequiredService<DiscordSocketClient>();
+    
+await services.GetRequiredService<LoggingService>().InitializeAsync();
+await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
 
-async Task MainAsync()
-{
-    using var services = ConfigureServices();
-    var client = services.GetRequiredService<DiscordSocketClient>();
-        
-    await services.GetRequiredService<LoggingService>().InitializeAsync();
-    await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+await client.LoginAsync(TokenType.Bot, token);
+await client.StartAsync();
 
-    var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
-    await client.LoginAsync(TokenType.Bot, token);
-    await client.StartAsync();
-
-    await Task.Delay(-1);
-}
-
-ServiceProvider ConfigureServices()
-{
-    return Startup.ConfigureServices().BuildServiceProvider();
-}
+Thread.Sleep(-1);
